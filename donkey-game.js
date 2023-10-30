ad9684934f33506daab4e87154b05cdd66b0ab3c
Lankz21 <31317325+Lankz21@users.noreply.github.com>
2023-10-30 05:03:44 -0400

    Update donkey-game.js

--- donkey-game.js
+++ donkey-game.js
@@ -1,6 +1,17 @@
-// Define the card suits and values
+// Game environment and state
+const players = [
+  { name: 'Player 1', hand: [] },
+  { name: 'Player 2', hand: [] },
+  // Add more players as needed
+];
+
 const cardSuits = ['Spades', 'Hearts', 'Clubs', 'Diamonds'];
 const cardValues = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
+
+let currentPlayerIndex = 0;
+let currentSuit = 'Spades';
+let highestCard = null;
+let collectedCards = [];
 
 // Function to create a standard deck of cards
 function createDeck() {
@@ -31,12 +42,101 @@ function dealCards(deck, players) {
   }
 }
 
+// Function to play a card
+function playCard(player, card) {
+  const cardIndex = player.hand.findIndex(c => c.suit === card.suit && c.value === card.value);
+  if (cardIndex !== -1) {
+    player.hand.splice(cardIndex, 1);
+    return card;
+  }
+  return null;
+}
+
+// Function to check for valid moves and determine the highest card played
+function determineHighestCard(cards) {
+  let highest = null;
+  for (let card of cards) {
+    if (card.suit === currentSuit) {
+      if (!highest || cardValues.indexOf(card.value) > cardValues.indexOf(highest.value)) {
+        highest = card;
+      }
+    }
+  }
+  return highest;
+}
+
+// Game logic - to be implemented
+function startGame() {
+  // Implement the game logic, round flow, player turns, picking up cards, valid moves, etc.
+}
+
 // Example usage
-const players = [{ name: 'Player 1', hand: [] }, { name: 'Player 2', hand: [] }];
 const deck = createDeck();
 const shuffledDeck = shuffleDeck(deck);
 dealCards(shuffledDeck, players);
+startGame(); // Start the game
 
-console.log('Players:');
-console.log(players[0].name, players[0].hand);
-console.log(players[1].name, players[1].hand);
+
+// Function to determine the next player
+function nextPlayer() {
+  currentPlayerIndex = (currentPlayerIndex + 1) % players.length;
+}
+
+// Function to simulate a player's turn (sample logic, should be expanded)
+function playerTurn() {
+  const currentPlayer = players[currentPlayerIndex];
+  const validMoves = currentPlayer.hand.filter(card => card.suit === currentSuit);
+
+  if (validMoves.length > 0) {
+    // Player plays a valid card based on current suit
+    const selectedCard = validMoves[0]; // Sample logic, actual game logic required
+    playCard(currentPlayer, selectedCard);
+
+    // Check if this card is the highest among played cards
+    if (!highestCard || cardValues.indexOf(selectedCard.value) > cardValues.indexOf(highestCard.value)) {
+      highestCard = selectedCard;
+    }
+
+    // Update current suit if Ace is played
+    if (selectedCard.value === 'Ace') {
+      currentSuit = selectedCard.suit;
+    }
+  } else {
+    // No valid move, player discards a card (sample logic)
+    const discardedCard = currentPlayer.hand[0]; // Sample logic, actual game logic required
+    playCard(currentPlayer, discardedCard);
+
+    // Player picks up the collected cards
+    currentPlayer.hand.push(...collectedCards);
+    collectedCards = [];
+  }
+
+  nextPlayer(); // Move to the next player
+}
+
+// Game logic - a simple example of rounds (needs more implementation)
+function playRounds() {
+  while (/* condition for continuing the game */) {
+    playerTurn(); // Simulate a turn for the current player
+
+    // Example condition for the end of a round
+    if (players.some(player => player.hand.length === 0)) {
+      // Round completed
+      // Determine scores, perform round-end actions, etc.
+    }
+  }
+}
+
+// Function to identify the "Donkey" (player with all cards)
+function identifyDonkey() {
+  const donkey = players.find(player => player.hand.length === 52);
+  return donkey;
+}
+
+// Start the game
+playRounds();
+
+// Identify the "Donkey" at the end of the game
+const donkeyPlayer = identifyDonkey();
+console.log(`The Donkey is: ${donkeyPlayer.name}`);
+
